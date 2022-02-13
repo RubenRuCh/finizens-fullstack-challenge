dbc6350cd4ba0ef194bac93b214aac86ba89acf2
Rubén Rüger <ruben_27895@hotmail.com>
2022-02-13 10:54:07 +0100

    Event listener should use command, not use case

--- src/Contexts/Investment/Portfolio/Application/Command/DeleteAllocation/DeleteAllocationWhenSharesIsZeroOnAllocationUpdated.ts
+++ src/Contexts/Investment/Portfolio/Application/Command/DeleteAllocation/DeleteAllocationWhenSharesIsZeroOnAllocationUpdated.ts
@@ -1,31 +1,41 @@
 import { DomainEventClass } from "../../../../../Shared/Domain/Event/DomainEvent";
 import { DomainEventSubscriber } from "../../../../../Shared/Domain/Event/DomainEventSubscriber";
-import { InvestmentAllocationId } from "../../../../Shared/Domain/ValueObject/InvestmentAllocationId";
-import { InvestmentPortfolioId } from "../../../../Shared/Domain/ValueObject/InvestmentPortfolioId";
 import { InvestmentShares } from "../../../../Shared/Domain/ValueObject/InvestmentShares";
 import { InvestmentAllocationUpdated } from "../../../Domain/Event/Allocation/InvestmentAllocationUpdated";
+import { DeleteInvestmentAllocationCommand } from "./DeleteInvestmentAllocationCommand";
+import { DeleteInvestmentAllocationCommandHandler } from "./DeleteInvestmentAllocationCommandHandler";
+import { DeleteInvestmentAllocationRequest } from "./DeleteInvestmentAllocationRequest";
 import { InvestmentAllocationEraser } from "./InvestmentAllocationEraser";
 
 export class DeleteAllocationWhenSharesIsZeroOnAllocationUpdated implements DomainEventSubscriber<InvestmentAllocationUpdated> {
-    constructor(private allocationEraser: InvestmentAllocationEraser) {}
   
-    subscribedTo(): DomainEventClass[] {
-      return [InvestmentAllocationUpdated];
+  private handler: DeleteInvestmentAllocationCommandHandler;
+
+  constructor(private allocationEraser: InvestmentAllocationEraser) {
+    this.handler = new DeleteInvestmentAllocationCommandHandler(this.allocationEraser);
+  }
+
+  subscribedTo(): DomainEventClass[] {
+    return [InvestmentAllocationUpdated];
+  }
+
+  async on(domainEvent: InvestmentAllocationUpdated): Promise<void> {
+    const { aggregateId, allocation} = domainEvent;
+
+    const shares = new InvestmentShares(allocation.shares);
+
+    if (!shares.areEmpty) {
+      return;
     }
-  
-    async on(domainEvent: InvestmentAllocationUpdated): Promise<void> {
-      const { aggregateId, allocation} = domainEvent;
 
-      const shares = new InvestmentShares(allocation.shares);
+    const request: DeleteInvestmentAllocationRequest = {
+      portfolioId: aggregateId,
+      allocationId: allocation.id,
+    };
 
-      if (!shares.areEmpty) {
-        return;
-      }
+    const command = new DeleteInvestmentAllocationCommand(request);
 
-      const portfolioId = new InvestmentPortfolioId(aggregateId);
-      const allocationId = new InvestmentAllocationId(allocation.id);
-  
-      return this.allocationEraser.run({ portfolioId, allocationId });
-    }
+    this.handler.handle(command)
+  }
 }
   

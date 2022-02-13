706f40e1cdbac0e655b4d61a631aeb0add2c4c56
Rubén Rüger <ruben_27895@hotmail.com>
2022-02-13 18:39:31 +0100

    Make query responses extends from response

--- src/Contexts/Investment/Order/Application/Query/SearchNonCompletedOrder/SearchInvestmentOrdersResponse.ts
+++ src/Contexts/Investment/Order/Application/Query/SearchNonCompletedOrder/SearchInvestmentOrdersResponse.ts
@@ -1,6 +1,7 @@
+import { QueryResponse } from './../../../../../Shared/Domain/CQRS/Query/QueryResponse';
 import { InvestmentOrder } from './../../../Domain/Model/InvestmentOrder';
 
-export class SearchInvestmentOrdersResponse {
+export class SearchInvestmentOrdersResponse implements QueryResponse {
   readonly orders: InvestmentOrder[];
 
   constructor(orders: InvestmentOrder[]) {
--- src/Contexts/Investment/Portfolio/Application/Query/FindPortfolio/FindInvestmentPortfolioResponse.ts
+++ src/Contexts/Investment/Portfolio/Application/Query/FindPortfolio/FindInvestmentPortfolioResponse.ts
@@ -1,6 +1,7 @@
+import { QueryResponse } from '../../../../../Shared/Domain/CQRS/Query/QueryResponse';
 import { InvestmentPortfolio } from './../../../Domain/Model/Portfolio/InvestmentPortfolio';
 
-export class FindInvestmentPortfolioResponse {
+export class FindInvestmentPortfolioResponse implements QueryResponse {
   readonly portfolio: InvestmentPortfolio;
 
   constructor(portfolio: InvestmentPortfolio) {
